79bff9aaf88e655f359aeee91c962eb82136a892
Elias Juarez <juarezelias83@gmail.com>
2023-11-05 19:02:24 -0300

    se agrego un condicional que comprueba que el numero ingresado este dentro del rango solicitado

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,8 +1,12 @@
 let numero = parseInt(prompt("ingrese un numero entre 1 y 50"));
 
-for(let f = 1; f <= numero; f++){
-    for(let c = 1; c <= f; c++){
-        document.write(c);
+if (numero >= 1 && numero <= 50) {
+  for (let f = 1; f <= numero; f++) {
+    for (let c = 1; c <= f; c++) {
+      document.write(c);
     }
     document.write("<br>");
+  }
+}else{
+    alert("El numero ingresado no es valido");
 }
