3a284717e6c605a503f5db6c81c691f8958c9e40
Elias Juarez <juarezelias83@gmail.com>
2023-11-05 01:54:15 -0300

    se modifico el ejercicio3

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,8 +1,9 @@
-do{
-  const texto1 = prompt("escriba un texto");
-  const texto2 = prompt("escriba otro texto");
+let texto = "";
+let textoUnido = "";
 
-  let textoUnido = texto1 + "-" + texto2 + "-";
-  document.write(textoUnido);
-  console.log(textoUnido);
-}while(confirm("quiere seguir escribiendo otro texto?"))
+do{
+  texto = prompt("escriba un texto");
+  texto = texto + "-";
+  textoUnido = textoUnido + texto;
+}while(confirm("quiere seguir escribiendo otro texto?"));
+document.write(textoUnido);
